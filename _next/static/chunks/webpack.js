484ebf08aa13bf6dbcfe74a79a0ee40287f00e11
Mon Nagata <mon_nagata@hotmail.com>
2021-03-01 23:36:47 +0900

    fix: 日付フォーマットを間違えてた

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9833e587e19361adb5d8";
+/******/ 	var hotCurrentHash = "e71ff9565b09c1fc8655";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
