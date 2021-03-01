1abd1276e6dce159d51666d3feb97a7bdf44d003
Mon Nagata <mon_nagata@hotmail.com>
2021-03-01 21:55:27 +0900

    feat: 履歴にリンク

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1a097b2cbdb22da66766";
+/******/ 	var hotCurrentHash = "9833e587e19361adb5d8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
